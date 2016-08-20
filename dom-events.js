f22301a677623429347fad0235516c79c118d118
Tom Wilson <tom@jackhq.com>
2016-08-20 08:28:56 -0400

    fixed undefined hash

--- dom-events.js
+++ dom-events.js
@@ -5,12 +5,15 @@ module.exports = function (store, event) {
   const click$ = most.fromEvent('click', window)
     .filter(e => e.srcElement.tagName === 'A')
     .tap(e => e.preventDefault())
-    .map(e => ({route: e.srcElement.pathname, hash: e.srcElement.hash }))
+    .map(e => ({
+      route: e.srcElement.pathname,
+      hash: e.srcElement.hash || ''
+    }))
 
   const pop$ = most.fromEvent('popstate', window)
     .map(e => ({
       route: window.location.pathname,
-      hash: window.location.hash 
+      hash: window.location.hash || ''
     }))
 
   // TODO: add default for form submit
@@ -18,6 +21,6 @@ module.exports = function (store, event) {
   const custom$ = most.fromEvent('send', store)
   return most.merge(d$, click$, pop$, custom$)
     .tap(event => {
-      window.history.pushState(null, '', event.route + event.hash)
+      window.history.pushState(null, '', event.route + (event.hash || ''))
     })
 }
